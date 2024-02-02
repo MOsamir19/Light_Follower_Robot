b355bd469c0bf77c4a5033dbd3eff991b494f537
MOsamir19 <mohamedsamirhashish@gmail.com>
2024-02-02 04:02:52 +0200

    first commit

--- light follower robot/main.c
+++ light follower robot/main.c
@@ -10,28 +10,14 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "util/delay.h"
-
 #include "DIO_Interface.h"
-#include "EXTINT_Interface.h"
-#include "UART_Interface.h"
-#include "SPI_Interface.h"
-#include "GIE_Interface.h"
 #include "ADC_Interface.h"
-#include "TMR0_Interface.h"
-#include "KPD_Interface.h"
 #include "LCD_Interface.h"
-#include "TIMER1_int.h"
-#include "TWI_int.h"
-#include "EXT_EEPROM_int.h"
-
-#include"string.h"
 
 #ifndef F_CPU
 #define F_CPU 8000000UL
 #endif
-//#include "avr/io.h"
-//#undef  F_CPU
-//#define F_CPU (16000000UL)
+
 
 
 
@@ -44,7 +30,7 @@ int main()
 	u16 DATA_OLD =5 ;
 	u16 MAX =0;
 	u8 i=0 ;
-	u8 y=4 ;
+	u8 select = 4 ;
 
 
 	LCD_voidInit();
@@ -73,14 +59,16 @@ int main()
 		{
 
 			if(DATA[i] > MAX)
-			{
+			   {
 				MAX = DATA[i]  ;
-				y = i ;
-			}
-			else if(MAX == DATA[i]) {
-				y = 4 ;
-			}
-			DATA_NEW =y ;
+				select = i ;
+			   }
+			else if(MAX == DATA[i]) 
+                            {
+				select = 4 ;
+			    }
+
+			DATA_NEW = select ;
 		}
 
 
@@ -89,36 +77,39 @@ int main()
 		{
 			LCD_voidClear();
 
-			switch(y)
+			switch(select)
 			{
 			case 0 : LCD_voidWriteString((u8*)"Moving Forward") ;
-			DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_HIGH);
-			DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
 
-			DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_HIGH);
-			DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
+			        DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_HIGH);
+			        DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
+
+			        DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_HIGH);
+			        DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
 			break ;
 
-			case 1 : LCD_voidWriteString((u8*)"Moving Right") ;
-			DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
-			DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
+			case 1 : LCD_voidWriteString((u8*)"Moving Right") ; 
 
-			DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_HIGH);
-			DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
+			         DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
+			         DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
+
+			         DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_HIGH);
+			         DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
 			break ;
 
-			case 2 :  LCD_voidWriteString((u8*)"Moving Back") ;
-			DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
-			DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_HIGH);
+			case 2 :   LCD_voidWriteString((u8*)"Moving Back") ;
 
-			DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_LOW);
-			DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_HIGH);
+			          DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
+			          DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_HIGH);
+
+			          DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_LOW);
+			          DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_HIGH);
 
 			break ;
 
 
-			case 3 :
-				LCD_voidWriteString((u8*)"Moving Left") ;
+			case 3 :   LCD_voidWriteString((u8*)"Moving Left") ;
+				
 				DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_HIGH);
 				DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
 
@@ -127,11 +118,12 @@ int main()
 				break ;
 
 			default :  LCD_voidWriteString((u8*)"STOP") ;
-			DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
-			DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
 
-			DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_LOW);
-			DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
+			        DIO_voidSetPinValue(PORTD_ID,PIN0,PIN_LOW);
+			        DIO_voidSetPinValue(PORTD_ID,PIN1,PIN_LOW);
+
+			        DIO_voidSetPinValue(PORTD_ID,PIN2,PIN_LOW);
+			        DIO_voidSetPinValue(PORTD_ID,PIN3,PIN_LOW);
 			break ;
 			}
 
